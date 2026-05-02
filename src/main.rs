b07224c846512b47032fb83b042a7a893b2976c0
Moritisimor <devMoritisimor@proton.me>
2026-05-02 16:51:40 +0200

    Added functio for detecting if a file is elf

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,20 @@ fn make_error(msg: &str) -> io::Error {
     io::Error::new(io::ErrorKind::Other, msg)
 }
 
+fn is_elf(data: &[u8]) -> bool {
+    data.starts_with(&[0x7f, 0x45, 0x4c, 0x46])
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let file_name = std::env::args()
         .nth(1)
         .ok_or(make_error("Please choose a file name"))?;
 
     let data = fs::read(file_name)?;
+    if is_elf(&data) {
+        println!("This file is probably elf.")
+    }
+    
     let mut idx = 0;
     for byte in data {
         idx += 1;
