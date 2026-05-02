155bda039ef0b9cb7b4b7e44fca77f79c28e1712
Moritisimor <devMoritisimor@proton.me>
2026-05-02 18:36:54 +0200

    Moved helper functions into their own file

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,9 @@
+mod helpers;
+
 use std::fs;
 
 use anyhow::bail;
 use owo_colors::OwoColorize;
-
-fn is_elf(data: &[u8]) -> bool {
-    data.starts_with(&[0x7f, 0x45, 0x4c, 0x46])
-}
-
-fn is_probably_printable(c: char) -> bool {
-    !c.is_control() && !c.is_whitespace()
-}
-
-/*
- * Weird name, but what this function does is detect whether a character
- * is something like a tab, newline, carriage return etc.
-*/
-fn is_special_control<'a>(c: u8) -> Option<&'a str> {
-    match c {
-        0x20 => Some("SPACE"),
-        0xa => Some("LINEFEED"),
-        0xd => Some("CARRIAGE RETURN"),
-        0x9 => Some("TAB"),
-        _ => None,
-    }
-}
 
 fn main() -> anyhow::Result<()> {
     let file_name = match std::env::args().nth(1) {
@@ -32,7 +12,7 @@ fn main() -> anyhow::Result<()> {
     };
 
     let data = fs::read(file_name)?;
-    if is_elf(&data) {
+    if helpers::is_elf(&data) {
         println!("{}", "This file is probably ELF!".blue())
     }
 
@@ -47,9 +27,9 @@ fn main() -> anyhow::Result<()> {
             byte.yellow()
         );
 
-        if is_probably_printable(byte as char) {
+        if helpers::is_probably_printable(byte as char) {
             print!("| ({})", (byte as char).cyan())
-        } else if let Some(kind) = is_special_control(byte) {
+        } else if let Some(kind) = helpers::is_special_control(byte) {
             print!("| [{}]", kind.cyan());
         }
 
