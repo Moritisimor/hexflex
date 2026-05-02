c70f4c76ce56d67024954283c947fdbbbae4ec7e
Moritisimor <devMoritisimor@proton.me>
2026-05-02 16:45:51 +0200

    Implemented basic program that reads file path from CLI args and displays each byte as binary, hex and decimal

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use std::{fs, io};
+
+fn make_error(msg: &str) -> io::Error {
+    io::Error::new(io::ErrorKind::Other, msg)
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let file_name = std::env::args()
+        .nth(1)
+        .ok_or(make_error("Please choose a file name"))?;
+
+    let data = fs::read(file_name)?;
+    let mut idx = 0;
+    for byte in data {
+        idx += 1;
+        print!("{idx}: {:#010b} | {:#x} | {}", byte, byte, byte);
+        if byte.is_ascii() {
+            print!(" ({})", byte as char)
+        }
+
+        println!()
+    }
+
+    Ok(())
+}
