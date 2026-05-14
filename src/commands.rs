86a32570ca9284a6553a1770cdac8f8201e041a6
Moritisimor <devMoritisimor@proton.me>
2026-05-14 18:49:05 +0200

    Added a few for_eachs because why not

--- src/commands.rs
+++ src/commands.rs
@@ -14,10 +14,10 @@ pub fn read_byte(buf: &Vec<u8>, args: &Vec<&str>) {
 
         None => {
             let mut idx = 0;
-            for byte in buf {
-                helpers::print_line(*byte, idx);
+            buf.iter().for_each(|b| {
+                helpers::print_line(*b, idx);
                 idx += 1;
-            }
+            });
 
             return;
         }
@@ -31,20 +31,18 @@ pub fn read_byte(buf: &Vec<u8>, args: &Vec<&str>) {
 
         Some(i) => match helpers::usize_of_str(i) {
             Some(end_idx) => {
-                for idx in start_idx..=end_idx {
-                    match buf.get(idx) {
-                        Some(byte) => helpers::print_line(*byte, idx),
-                        None => {
-                            println!(
-                                "{} {}",
-                                "Error while printing range of bytes:".red(),
-                                "No such index in buffer"
-                            );
+                (start_idx..=end_idx).for_each(|idx| match buf.get(idx) {
+                    Some(byte) => helpers::print_line(*byte, idx),
+                    None => {
+                        println!(
+                            "{} {}",
+                            "Error while printing range of bytes:".red(),
+                            "No such index in buffer"
+                        );
 
-                            return;
-                        }
+                        return;
                     }
-                }
+                });
             }
 
             None => {
