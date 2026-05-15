233d4e6a02759d01c8b3bf73320f81291c794387
Moritisimor <devMoritisimor@proton.me>
2026-05-15 14:15:27 +0200

    Edit command now inserts the number as hex instead of decimal

--- src/commands.rs
+++ src/commands.rs
@@ -95,7 +95,7 @@ pub fn edit_byte(buf: &mut Vec<u8>, args: &Vec<&str>) {
     loop {
         let input = match rl.readline_with_initial(
             &format!("{} {} {} ", "Edit".green(), idx.blue(), ">".green()),
-            (&format!("{val}"), ""),
+            (&format!("{:#004x}", val), ""),
         ) {
             Ok(i) => i,
             Err(rustyline::error::ReadlineError::Eof)
