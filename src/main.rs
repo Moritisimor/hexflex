bffc4e4773a4ed0ac046cb36fd87d029bac9d42e
Moritisimor <devMoritisimor@proton.me>
2026-05-15 19:32:23 +0200

    Added option to save buffer to different file with save command

--- src/main.rs
+++ src/main.rs
@@ -84,7 +84,7 @@ fn main() -> anyhow::Result<()> {
             "quit" | "exit" | "q" => break,
             "read" | "r" => commands::read::read_byte(&data, &fields),
             "edit" | "e" => commands::edit::edit_byte(&mut data, &fields),
-            "save" | "s" => commands::save::save(&data, &flags.input_file),
+            "save" | "s" => commands::save::save(&data, &fields, &flags.input_file),
             "clear" | "c" => println!("\x1b[H\x1b[2J\x1b[3J"),
             _ => println!("{} {}", "Unknown command:".red(), fields[0].blue()),
         }
