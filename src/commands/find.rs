89bbcd5191ca9829e25b2297773d372e64def2ab
Moritisimor <devMoritisimor@proton.me>
2026-05-17 04:37:05 +0200

    Cleaned up copy-pasted code

--- src/commands/find.rs
+++ src/commands/find.rs
@@ -2,30 +2,19 @@ use owo_colors::OwoColorize;
 
 use crate::helpers;
 
-pub fn find_string(buf: &Vec<u8>, args: &Vec<&str>) {
-    let search_bytes: Vec<u8> = args[1..].join(" ").chars().map(|c| c as u8).collect();
-
-    if search_bytes.is_empty() {
-        println!(
-            "{}\n{}",
-            "Invalid amount of arguments.".red(),
-            "Usage: find <bytes...>".green()
-        );
-
-        return;
-    }
-
+// Finds the needle in the haystack
+fn find(needle: &[u8], haystack: &[u8]) {
     println!("{}", "Searching...".green());
     let mut matches = 0;
     let mut idx = 0;
-    for b in buf {
-        if *b == search_bytes[0] {
-            if buf[idx..].starts_with(&search_bytes) {
+    for b in haystack {
+        if *b == needle[0] {
+            if haystack[idx..].starts_with(needle) {
                 println!(
                     "{} {:#010x} - {:#010x}",
                     "Match found at:".green(),
                     idx.blue(),
-                    (idx + search_bytes.len() - 1).blue()
+                    (idx + needle.len() - 1).blue()
                 );
 
                 matches += 1;
@@ -40,6 +29,22 @@ pub fn find_string(buf: &Vec<u8>, args: &Vec<&str>) {
         1 => println!("{} {}", 1.blue(), "Match!".green()),
         _ => println!("{} {}", matches.blue(), "Matches!".green()),
     }
+}
+
+pub fn find_string(buf: &Vec<u8>, args: &Vec<&str>) {
+    let search_bytes: Vec<u8> = args[1..].join(" ").chars().map(|c| c as u8).collect();
+
+    if search_bytes.is_empty() {
+        println!(
+            "{}\n{}",
+            "Invalid amount of arguments.".red(),
+            "Usage: find <bytes...>".green()
+        );
+
+        return;
+    }
+
+    find(&search_bytes, &buf)
 }
 
 pub fn find_bytes(buf: &Vec<u8>, args: &Vec<&str>) {
@@ -61,40 +66,5 @@ pub fn find_bytes(buf: &Vec<u8>, args: &Vec<&str>) {
         }
     };
 
-    // I'll clean copy-paste up soon I promise
-    if search_bytes.is_empty() {
-        println!(
-            "{}\n{}",
-            "Invalid amount of arguments.".red(),
-            "Usage: find <bytes...>".green()
-        );
-
-        return;
-    }
-
-    println!("{}", "Searching...".green());
-    let mut matches = 0;
-    let mut idx = 0;
-    for b in buf {
-        if *b == search_bytes[0] {
-            if buf[idx..].starts_with(&search_bytes) {
-                println!(
-                    "{} {:#010x} - {:#010x}",
-                    "Match found at:".green(),
-                    idx.blue(),
-                    (idx + search_bytes.len() - 1).blue()
-                );
-
-                matches += 1;
-            }
-        }
-
-        idx += 1;
-    }
-
-    match matches {
-        0 => println!("{}", "No matches for this byte sequence".red()),
-        1 => println!("{} {}", 1.blue(), "Match!".green()),
-        _ => println!("{} {}", matches.blue(), "Matches!".green()),
-    }
+    find(&search_bytes, &buf)
 }
